e8682280545962a2d88d13313a230af221099b03
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-01 01:10:12 +0530

    Added Line UC3 program

---
+++ src/com/lineproblem/test/Line_ComparisionUc3.java
@@ -0,0 +1,52 @@
+package com.lineproblem.test;
+
+import java.util.Scanner;
+
+public class Line_ComparisionUc3 {
+	public static void main(String[] args) {
+
+	int x1=0,x2=0,y1=0,y2=0,a1,a2,b1,b2;
+	
+	Scanner sc= new Scanner(System.in);
+	System.out.println("enter the value x1:");		
+	x1=sc.nextInt();
+	System.out.println("enter the value y1:");
+	y1=sc.nextInt();
+	System.out.println("enter the value x2:");
+	x2=sc.nextInt();
+	System.out.println("enter the value y2:");
+	y2=sc.nextInt();
+	
+	int length1 =(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	
+	Integer L1=length1;
+	System.out.println("enter the value a1:");
+	a1=sc.nextInt();
+	System.out.println("enter the value b1:");
+	b1=sc.nextInt();
+	System.out.println("enter the value a2:");
+	a2=sc.nextInt();
+	System.out.println("enter the value b2:");
+	b2=sc.nextInt();
+	
+	int length2= (int)Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+	
+	Integer L2=length2;
+	int res= L1.compareTo(L2);
+	
+	if(res==0)
+	{
+	System.out.println("Two lines are equal ");
+    }
+	else if(res>0)
+	{
+ System.out.println(" The line1 is greater than line 2 ");
+	}
+	else
+	{
+		System.out.println(" The line1 is lessthan line2 ");
+	}
+	
+
+   }
+}
